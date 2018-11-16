50f23da36b1dd47b0029bf9ee322f6c8eb17a618
glaracuente <glaracuente@outbrain.com>
2018-11-15 23:38:40 -0500

    updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -88,3 +88,19 @@ function readProducts() {
     connection.end();
   });
 }
+
+function queryAllSongs() {
+    connection.query("SELECT * FROM songs", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+      }
+      console.log("-----------------------------------");
+    });
+  }
+  
+  function queryDanceSongs() {
+    var query = connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+      }
+    });
